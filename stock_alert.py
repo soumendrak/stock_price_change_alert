136fbfe06f2ef620e59b36ea7a41e55f75f9cd45
Soumendra kumar sahoo <soumendra.s@outlook.com>
2017-04-23 12:22:30 +0530

    Try Except block added.

--- stock_alert.py
+++ stock_alert.py
@@ -7,6 +7,7 @@ Future scope: If stock price decreased/increased continuously (< 5 %)
 Author:     Soumendra Kumar Sahoo
 Created on: 23 April 2017
 """
+from __future__ import print_function
 import json
 from urllib import urlopen
 from customizable_data import defined_limit, watchlist, number
@@ -19,16 +20,20 @@ def quote_grab(symbol):
     """
     Grab the stock and related information.
     :return:
-        Stock price in float format
         Percentage changes in the share price
     """
-    base_url = 'http://finance.google.com/finance/info?client=ig&q='
-    url_data = urlopen(base_url + symbol)
-    # [3:] is done to avoidthe initial `//`
-    google_finance_data = json.loads(url_data.read().decode('ascii', 'ignore').strip()[3:])
-    share_price_changes = float(google_finance_data[0]['cp_fix'])
-    print ('Checking quotes for {}'.format(symbol))
-    return share_price_changes
+    try:
+        base_url = 'http://finance.google.com/finance/info?client=ig&q='
+        url_data = urlopen(base_url + symbol)
+        # [3:] is done to avoidthe initial `//`
+        google_finance_data = json.loads(url_data.read().decode(
+            'ascii', 'ignore').strip()[3:])
+        share_price_changes = float(google_finance_data[0]['cp_fix'])
+        print ('Checking quotes for {}'.format(symbol))
+        return share_price_changes
+    except Exception as error_message:
+        print('Parsing the share failed:', error_message)
+        return None
 
 
 def main():
@@ -36,22 +41,25 @@ def main():
     Invoke each share and trigger for SMS if the share price
     changed more than the defined percentage.
     """
-    for each_share in watchlist:
-        quote_price_change = quote_grab(each_share)
-        print('{}:{}'.format(each_share, quote_price_change))
-        if abs(quote_price_change) > defined_limit:
-            if quote_price_change > 0:
-                message = each_share + ' price has UP by ' + \
-                    str(abs(quote_price_change)) + ' percent.'
-            else:
-                message = each_share + ' price has DOWN by ' + \
-                    str(abs(quote_price_change)) + ' percent.'
-            print('sending SMS for {}'.format(each_share))
-            sms_status = send_sms(message, number)
-            if sms_status:
-                print('SMS for {} sent successfully'.format(each_share))
-            else:
-                print('SMS for {} Failed to go'.format(each_share))
+    try:
+        for each_share in watchlist:
+            quote_price_change = quote_grab(each_share)
+            print('{}:{}'.format(each_share, quote_price_change))
+            if abs(quote_price_change) > defined_limit:
+                if quote_price_change > 0:
+                    message = each_share + ' price has UP by ' + \
+                        str(abs(quote_price_change)) + ' percent.'
+                else:
+                    message = each_share + ' price has DOWN by ' + \
+                        str(abs(quote_price_change)) + ' percent.'
+                print('sending SMS for {}'.format(each_share))
+                sms_status = send_sms(message, number)
+                if sms_status:
+                    print('SMS for {} sent successfully'.format(each_share))
+                else:
+                    print('SMS for {} Failed to go'.format(each_share))
+    except Exception as error_message:
+        print('Calculating the share percentage failed:', error_message)
 
 
 if __name__ == '__main__':
